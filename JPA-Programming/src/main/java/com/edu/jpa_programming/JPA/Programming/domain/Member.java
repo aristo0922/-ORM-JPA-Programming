2d2acdc39183de952b9048f1e7d333bf71e6bb83
aristo <dkfud2121@naver.com>
2024-12-27 14:55:58 +0900

    1.1.1 반복,  반복, 그리고 반복

---
+++ JPA-Programming/src/main/java/com/edu/jpa_programming/JPA/Programming/domain/Member.java
@@ -0,0 +1,8 @@
+package com.edu.jpa_programming.JPA.Programming.domain;
+
+public class Member {
+
+  private String memberId;
+  private String name;
+
+}
---
+++ JPA-Programming/src/main/java/com/edu/jpa_programming/JPA/Programming/domain/MemberDAO.java
@@ -0,0 +1,13 @@
+package com.edu.jpa_programming.JPA.Programming.domain;
+
+public class MemberDAO {
+  public Member find(String memberId){
+    //...
+    return null;
+  }
+
+  public void save(Member member){
+    //...
+  }
+
+}
