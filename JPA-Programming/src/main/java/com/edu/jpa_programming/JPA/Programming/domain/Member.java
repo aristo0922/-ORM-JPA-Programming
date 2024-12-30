f8a40922e4afa082e1feab70f75b71e178252941
aristo <dkfud2121@naver.com>
2024-12-30 18:32:52 +0900

    ch02. prac2.6 매핑 정보가 포함된 회원 클래스

--- JPA-Programming/src/main/java/com/edu/jpa_programming/JPA/Programming/domain/Member.java
+++ JPA-Programming/src/main/java/com/edu/jpa_programming/JPA/Programming/domain/Member.java
@@ -1,8 +1,35 @@
 package com.edu.jpa_programming.JPA.Programming.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name ="MEMBER")
 public class Member {
 
-  private String memberId;
-  private String name;
+  @Id
+  @Column(name = "ID")
+  private String id;
+  @Column(name = "NAME")
+  private String username;
 
+  private Integer age;
+
+  public String getId() {
+    return id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
 }
--- JPA-Programming/src/main/java/com/edu/jpa_programming/JPA/Programming/domain/MemberDAO.java
+++ JPA-Programming/src/main/java/com/edu/jpa_programming/JPA/Programming/domain/MemberDAO.java
@@ -1,13 +1,39 @@
 package com.edu.jpa_programming.JPA.Programming.domain;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberDAO {
-  public Member find(String memberId){
-    //...
-    return null;
+
+  public void setDataSource(DataSource dataSource) {
+    this.jdbcTemplate = new JdbcTemplate(dataSource);
   }
 
-  public void save(Member member){
-    //...
+  private JdbcTemplate jdbcTemplate;
+
+  private RowMapper<Member> memberMapper = new RowMapper<Member>() {
+    @Override
+    public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+      Member member = new Member();
+      member.setId(rs.getString("MEMBER_ID"));
+      member.setUsername(rs.getString("NAME"));
+      return member;
+    }
+  };
+
+  public Member find(String memberId) {
+    return this.jdbcTemplate.queryForObject("SELECT * FROM MEMBER WHERE MEMBER_ID = ?",
+        new Object[]{memberId}, this.memberMapper);
+  }
+
+  public void save(Member member) {
+    String sql = "INSERT INTO MEMBER(MEMBER_ID, NAME) VALUES(?, ?)";
+    this.jdbcTemplate.update(sql, member.getId(), member.getUsername());
   }
 
 }
